1718e2948b192a1a75ccbdd5278fcf83ca05298b
tinfoil-knight <kunal99kundu@gmail.com>
2020-11-01 23:57:45 +0530

    refactor: separated handlers; fix: code execution after error

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,69 +16,72 @@ import (
 )
 
 type application struct {
-	db *bolt.DB
+	db     *bolt.DB
+	bucket []byte
 }
 
-func (app *application) apiHandler(w http.ResponseWriter, r *http.Request) {
-
-	bucket := []byte("links")
+func (app *application) getHandler(w http.ResponseWriter, r *http.Request) {
 	var validate *validator.Validate
 	validate = validator.New()
-	if r.URL.Path == "/" {
-		if r.Method != "POST" {
-			sendError(w, http.StatusMethodNotAllowed)
-			return
-		}
-		body, err := ioutil.ReadAll(r.Body)
-		url := body
-		shortCode := helpers.GenerateShortString()
-		err = app.db.Update(func(tx *bolt.Tx) error {
-			b, err := tx.CreateBucketIfNotExists(bucket)
-			if err != nil {
-				return err
-			}
-			// TODO: Protect against crash/infinite loop by limiting num of iterations
-			// TODO: Test this loop
-			for {
-				chk := b.Get(shortCode)
-				if chk == nil {
-					break
-				}
-				shortCode = helpers.GenerateShortString()
-			}
-			return b.Put(shortCode, url)
-		})
-		if err != nil {
-			sendError(w, http.StatusInternalServerError)
-		}
 
-		fmt.Fprintf(w, "%s\n", shortCode)
-
-	} else {
-		if r.Method != "GET" {
-			sendError(w, http.StatusMethodNotAllowed)
-		}
-		shortCode := []byte(r.URL.Path[1:])
-
-		err := validate.Var(string(shortCode), "required,alphanum,len=6")
-		if err != nil {
-			sendError(w, http.StatusBadRequest)
-		}
-		err = app.db.View(func(tx *bolt.Tx) error {
-			b := tx.Bucket(bucket)
-			url := b.Get([]byte(shortCode))
-			if url != nil {
-				fmt.Fprintf(w, "%s\n", url)
-			} else {
-				sendError(w, http.StatusBadRequest)
-			}
-			return nil
-		})
-		if err != nil {
-			sendError(w, http.StatusInternalServerError)
-		}
-
+	if r.Method != "GET" {
+		sendError(w, http.StatusMethodNotAllowed)
+		return
 	}
+	shortCode := []byte(r.URL.Path[1:])
+
+	err := validate.Var(string(shortCode), "required,alphanum,len=6")
+	if err != nil {
+		sendError(w, http.StatusBadRequest)
+		return
+	}
+	err = app.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(app.bucket)
+		url := b.Get([]byte(shortCode))
+		if url != nil {
+			fmt.Fprintf(w, "%s\n", url)
+		} else {
+			return errors.New("Not Found")
+		}
+		return nil
+	})
+	if err != nil {
+		if err.Error() == "Not Found" {
+			sendError(w, http.StatusNotFound)
+		} else {
+			sendError(w, http.StatusInternalServerError)
+		}
+	}
+}
+
+func (app *application) setHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		sendError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	url := body
+	shortCode := helpers.GenerateShortString()
+	err = app.db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists(app.bucket)
+		if err != nil {
+			return err
+		}
+		// TODO: Protect against crash/infinite loop by limiting num of iterations
+		// TODO: Test this loop
+		for {
+			chk := b.Get(shortCode)
+			if chk == nil {
+				break
+			}
+			shortCode = helpers.GenerateShortString()
+		}
+		return b.Put(shortCode, url)
+	})
+	if err != nil {
+		sendError(w, http.StatusInternalServerError)
+	}
+	fmt.Fprintf(w, "%s\n", shortCode)
 }
 
 func sendError(w http.ResponseWriter, statusCode int) {
@@ -100,11 +104,14 @@ func main() {
 	defer db.Close()
 
 	app := &application{
-		db: db,
+		db:     db,
+		bucket: []byte("links"),
 	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.apiHandler)
+
+	mux.HandleFunc("/new", app.setHandler)
+	mux.HandleFunc("/", app.getHandler)
 
 	log.Printf("INFO: Starting server on %s", httpPort)
 	log.Fatal(http.ListenAndServe(httpPort, logRequest(mux)))
